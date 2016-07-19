d8b3e9d34fb15bbe017bfc2497690f8f7cc90e87
Ivano Alvino <ivano.alvino@gmail.com>
2016-07-19 22:53:05 +0200

    Add initial menu in SplitUP class
    The initial menu with the standard operation has been created

--- src/com/ivano/splitup/SplitUP.java
+++ src/com/ivano/splitup/SplitUP.java
@@ -15,6 +15,24 @@ public class SplitUP {
     private ArrayList<Debit> listDebits;
 
     public static void main(String[] args) {
-        System.out.print("Hey you, use SplitUP!");
+
+        printMenu();
+
+    }
+
+    private static void printMenu() {
+        System.out.println("Menu\n----\n");
+        System.out.println("a - Add shared expense");
+        System.out.println("r - Remove shared expense");
+        System.out.println("m - Modify shared expense");
+        System.out.println();
+        System.out.println("u - Add a user");
+        System.out.println("x - Remove a user");
+        System.out.println();
+        System.out.println("d - Add a debit");
+        System.out.println("b - Remove a debit");
+        System.out.println();
+        System.out.println("c - Calculate total");
+
     }
 }
