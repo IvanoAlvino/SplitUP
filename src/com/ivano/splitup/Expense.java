167319c06524ca9713b6b4d99af1370cd7fbca54
ialvino <ivano.alvino@amadeus.com>
2016-07-20 12:29:29 +0200

    Add getters and setters to Expense class

--- src/com/ivano/splitup/Expense.java
+++ src/com/ivano/splitup/Expense.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 
 class Expense extends Transaction {
 
-  public Expense(User payer, double amount) {
-    this.payer = payer;
-    this.contributors = new ArrayList<User>();
-    this.amount = amount;
-  }
-
   /**
    * The list of users that share this expense.
    * All the contributors will share an equal part of the expense.
@@ -21,4 +15,26 @@ class Expense extends Transaction {
    * The person who payed for this expense.
    */
   private User payer;
+
+  public Expense(User payer, double amount) {
+    this.payer = payer;
+    this.contributors = new ArrayList<User>();
+    this.amount = amount;
+  }
+
+  /**
+   * Add a new contributor to {@link Expense#contributors}.
+   * @param contributor The user to add to the contributors list
+   */
+  public void addContributor(User contributor) {
+    this.contributors.add(contributor);
+  }
+
+  public ArrayList<User> getContributors() {
+    return this.contributors;
+  }
+
+  public String getPayerName() {
+    return this.payer.toString();
+  }
 }
