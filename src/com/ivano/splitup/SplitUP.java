bd2ed269a20299773b3e38c833f3848441765bd2
ialvino <ivano.alvino@amadeus.com>
2016-07-20 11:47:24 +0200

    Sanitize input for int values and double amounts
    - Introduced new methods readInt() and readDoubleAmount() to safely read from stdin integers and doubles
    - Created some small print menu methods

--- src/com/ivano/splitup/SplitUP.java
+++ src/com/ivano/splitup/SplitUP.java
@@ -28,34 +28,57 @@ public class SplitUP {
      * @return A User, either new or selected from existing one
      */
     private static User chooseUserFromListOrCreateNewUser() {
-        System.out.println("1 - Choose user from list");
-        System.out.println("2 - Create new user");
+        printChooseUserORCreateNewMenu();
 
         while (true) {
-            int choice = scanner.nextInt();
+            int choice = readInt();
             switch (choice) {
                 case 1:
                     return chooseUserFromList();
                 case 2:
                     return createNewUser();
                 default:
+                    printChooseUserORCreateNewMenu();
                     break;
             }
         }
     }
 
     /**
+     * Print a small menu used in {@link SplitUP#chooseUserFromListOrCreateNewUser}.
+     */
+    private static void printChooseUserORCreateNewMenu() {
+        System.out.println("1 - Choose user from list");
+        System.out.println("2 - Create new user");
+      }
+
+    /**
      * Select an existing user from {@link SplitUP#listUsers}.
      * @return A User
      */
     private static User chooseUserFromList() {
-        int selectedUser;
-        printUserList();
-        selectedUser = scanner.nextInt();
+        int selectedUser = -1;
+        while (selectedUser < 0 || selectedUser > listUsers.size() -1) {
+            printUserList();
+            selectedUser = readInt();
+        }
         return listUsers.get(selectedUser);
     }
 
-    /**
+  /**
+   * This method will safely read an int value.
+   * In case something different from an int is introduced, ask again for an int.
+   * @return A Integer value
+   */
+  private static int readInt() {
+    while (!scanner.hasNextInt()) {
+        System.out.println("Please insert a number");
+        scanner.next();
+    }
+    return scanner.nextInt();
+  }
+
+  /**
      * Print all the users from {@link SplitUP#listUsers}.
      */
     private static void printUserList() {
@@ -109,9 +132,9 @@ public class SplitUP {
     }
 
     /**
-     * This method will create and add a new expense to {@link SplitUP#listExpenses}.<br><br>
+     * This method will create and add a new expense to {@link SplitUP#listExpenses}.<br>
      * It asks to specify the paying <b>User</b>, the <b>Amount</b> that is being payed and the
-     * <b>Contributors</b> that will share the expense.<br><br>
+     * <b>Contributors</b> that will share the expense.<br>
      * The paying user and the contributors can either be created on the fly, or selected from a list of
      * already existing users.
      */
@@ -127,10 +150,24 @@ public class SplitUP {
             payer = chooseUserFromListOrCreateNewUser();
         }
 
-        System.out.println("How much?");
-        amount = scanner.nextDouble();
+        amount = readDoubleAmount();
         //TODO who is he sharing with?
         listExpenses.add(new Expense(payer, amount));
+    }
+
+    /**
+     * This method will safely read a double amount of money.
+     * In case something different from a double is introduced, ask again for a double.
+     * @return A Double representing an amount of money
+     */
+    private static Double readDoubleAmount() {
+        System.out.println("How much?");
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Please insert only digits");
+            System.out.println("How much?");
+            scanner.next();
+        }
+        return scanner.nextDouble();
     }
 
     public static void main(String[] args) {
@@ -178,6 +215,10 @@ public class SplitUP {
                 case "e":
                     System.out.println("Exit from SplitUP");
                     finished = true;
+                    break;
+
+                default:
+                    displayChoiceMessage();
                     break;
             }
         }
