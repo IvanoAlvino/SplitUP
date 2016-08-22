5c87093aded632abe2f445525ffd3de3430f8f3b
ialvino <ivano.alvino@amadeus.com>
2016-08-22 17:11:43 +0200

    Add performOperation method in Menu class

--- src/com/ivano/splitup/Menu.java
+++ src/com/ivano/splitup/Menu.java
@@ -1,38 +1,97 @@
 package com.ivano.splitup;
 
-public enum Menu {
-    A("Add shared expense"),
-    R("Remove shared expense"),
-    M("Modify shared expense"),
-    D("Add a debit"),
-    B("Remove a debit"),
-    C("Calculate total"),
-    E("Exit from SplitUp");
+enum Menu {
+  A("Add shared expense"),
+  R("Remove shared expense"),
+  M("Modify shared expense"),
+  D("Add a debit"),
+  B("Remove a debit"),
+  C("Calculate total"),
+  E("Exit from SplitUp");
 
-    private final String description;
+  private final String description;
 
-    private static final String SEPARATOR = " - ";
+  private static final String SEPARATOR = " - ";
 
-    Menu(String s) {
-        this.description = s;
+  Menu(String s) {
+    this.description = s;
+  }
+
+  /**
+   * Print a menu with possible operations.
+   */
+  public static void printMenu() {
+    IOManager.printStatusMessage("Menu");
+    for (Menu m : Menu.values()) {
+      System.out.println(m + SEPARATOR + m.description);
+    }
+  }
+
+  /**
+   * Display a simple message.
+   * Used every iteration of the main loop in main method.
+   */
+  public static void displayChoiceMessage() {
+    System.out.println();
+    System.out.print("Enter choice: ");
+  }
+
+  /**
+   * Perform a given Menu operation.
+   *
+   * @param choice
+   *         The given operation to perform
+   *
+   * @return A boolean value corresponding to true if exit operation is performed
+   */
+  public static boolean performOperation(Menu choice) {
+    boolean finished = false;
+
+    switch (choice) {
+    case A:
+      try {
+        ExpenseManager.addExpense();
+      }
+      catch (UsernameNotValidException e) {
+        UserManager.usernameNotAllowedErrorMessage();
+        IOManager.printStatusMessage("Expense creation interrupted");
+      }
+      break;
+
+    case R:
+      System.out.println("Remove a shared expense");
+      break;
+
+    case M:
+      System.out.println("Modify a shared expense");
+      break;
+
+    case D:
+      try {
+        ExpenseManager.addDebit();
+      }
+      catch (UsernameNotValidException e) {
+        UserManager.usernameNotAllowedErrorMessage();
+        IOManager.printStatusMessage("Debit creation interrupted");
+      }
+      break;
+
+    case B:
+      System.out.println("Remove a debit");
+      break;
+
+    case C:
+      UserManager.calculateAndShowResults();
+      break;
+
+    case E:
+      IOManager.printStatusMessage("Bye Bye");
+      finished = true;
+
+    default:
+      break;
     }
 
-    /**
-     * Print a menu with possible operations.
-     */
-    public static void printMenu() {
-        IOManager.printStatusMessage("Menu");
-        for (Menu m : Menu.values()) {
-            System.out.println(m + SEPARATOR + m.description);
-        }
-    }
-
-    /**
-     * Display a simple message.
-     * Used every iteration of the main loop in main method.
-     */
-    public static void displayChoiceMessage() {
-        System.out.println();
-        System.out.print("Enter choice: ");
-    }
+    return finished;
+  }
 }
--- src/com/ivano/splitup/SplitUP.java
+++ src/com/ivano/splitup/SplitUP.java
@@ -10,49 +10,7 @@ public class SplitUP {
 
       try {
         Menu choice = IOManager.getChoice();
-        switch (choice) {
-          case A:
-            try {
-              ExpenseManager.addExpense();
-            } catch (UsernameNotValidException e) {
-              UserManager.usernameNotAllowedErrorMessage();
-              IOManager.printStatusMessage("Expense creation interrupted");
-            }
-            break;
-
-          case R:
-            System.out.println("Remove a shared expense");
-            break;
-
-          case M:
-            System.out.println("Modify a shared expense");
-            break;
-
-          case D:
-            try {
-              ExpenseManager.addDebit();
-            } catch (UsernameNotValidException e) {
-              UserManager.usernameNotAllowedErrorMessage();
-              IOManager.printStatusMessage("Debit creation interrupted");
-            }
-            break;
-
-          case B:
-            System.out.println("Remove a debit");
-            break;
-
-          case C:
-            UserManager.calculateAndShowResults();
-            break;
-
-          case E:
-            IOManager.printStatusMessage("Bye Bye");
-            finished = true;
-            break;
-
-          default:
-            break;
-        }
+        finished = Menu.performOperation(choice);
       }
       catch (IllegalArgumentException e) {
         IOManager.printStatusMessage("Retry");
